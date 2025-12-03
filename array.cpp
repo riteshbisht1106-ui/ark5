56d0dbf757012ddbfe7265f289176183b60fe345
riteshbisht1106-ui <riteshbisht1106@gmail.com>
2025-12-03 14:11:41 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main () { 
+  char arr[5] = {'a','b','c','d'} ;
+  cout <<"array elements are: ";
+  for (int i = 0 ; i<5; i++) 
+  cout<<arr[i]<<" ";
+
+}
